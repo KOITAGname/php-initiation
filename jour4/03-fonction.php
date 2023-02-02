8e9b1a85419e9854d05348f1d027e7471c37a453
Gname K <gname.koita@yahoo.fr>
2023-02-02 13:38:16 +0100

    $_get mini_site

--- jour4/03-fonction.php
+++ jour4/03-fonction.php
@@ -1,5 +1,5 @@
 <?php 
-declare (strict_types=1);
+declare(strict_types=1);
 
 function genererArticle (array $article) : string {
     return "
@@ -7,6 +7,7 @@ function genererArticle (array $article) : string {
             <h2 class='card-header'>{$article["titre"]}</h2>
             <img src='{$article["img"]}' alt='' >
             <p>{$article["contenu"]}</p>
+        <a href='http://localhost/php-initiation/jour4/03-index.php?id={$article["id"]}'>lire la suite</a>
         </article>
     ";
 }
