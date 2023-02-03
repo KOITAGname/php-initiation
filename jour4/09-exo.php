99d1d913815db36d1610b9b3e127370bf4c420d5
Gname K <gname.koita@yahoo.fr>
2023-02-03 10:04:41 +0100

    fin exo-9

--- jour4/09-exo.php
+++ jour4/09-exo.php
@@ -4,7 +4,8 @@ require "09-data-variable.php";
 
 //http://localhost/php-initiation/jour4/09-exo.php
 
-if(!empty($_GET)){
+if(!empty($_GET) &&
+ isset($_GET["nom"])){////nous permet de verifier si les deux conditions sont vraie 
     $nom =$_GET["nom"];
     $new_ville = [];
     foreach($villes as $v){
@@ -29,21 +30,32 @@ $villes = $new_ville;
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
 </head>
 <body>
+    <header>
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="http://localhost/php-initiation/jour4/09-exo.php"></a></li>
+                    <li><a href="http://localhost/php-initiation/jour4/09-exo.php/nom=Paris"></a></li>
+                    <li><a href="http://localhost/php-initiation/jour4/09-exo.php/nom=Grenoble0"></a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 
 <div class="container">
         <section class="row">
             <!--  3 cas 
-                plusieurs villes
-                1 seul villes
-                0 villes
-                http://localhost/php-initiation/jour4/08-exo.php
+                afficher plusieurs villes
+                affivher une seul villes
+                afficher une ville inconnu
+                http://localhost/php-initiation/jour4/09-exo.php
             --> 
 <!-- -------------------- plusieurs villes------------------- -->
 <?php if(count($villes) > 1) : ?>
                 <h1>plusieurs villes</h1>
                 <?php foreach($villes as $v) : ?>
                     <p>nom : <?php echo $v["nom"] ?></p>
-                    <p>nbHabitant : <?php echo $v["nbHabitant"] ?></p>
+                    <p>nbHabitant : <?php echo number_format( $v["nbHabitant"],0,";"," ") ?></p>
                     <hr>
                 <?php endforeach ?>
                 
@@ -51,7 +63,7 @@ $villes = $new_ville;
 <?php elseif(count($villes) === 1) : ?>
                 <h1>un seul villes</h1>
                 <p>nom : <?php echo $villes[0]["nom"] ?></p>
-                <p>nbHabitant : <?php echo $villes[0]["nbHabitant"] ?></p>
+                <p>nbHabitant : <?php echo number_format( $villes[0]["nbHabitant"] ,0,";"," ") ?></p>
             
 <!---------------------aucun villes ----------------------->
 <?php else : ?>
