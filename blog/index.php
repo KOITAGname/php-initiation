53ccb54b6d19f7185731a390c30386f7f20b8d8f
Gname K <gname.koita@yahoo.fr>
2023-02-03 16:52:42 +0100

    blog-debut

---
+++ blog/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li class="nav-item">
+                    <a href="http://localhost/php-initiation/jour5/index.php?" class="nav-link">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a href="http://localhost/php-initiation/jour5/index.php?page=contact" class="nav-link">Nous contacter</a>
+                </li>
+                <li class="nav-item">
+                    <a href="http://localhost/php-initiation/jour5/index.php?page=login" class="nav-link">Connexion</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    
+</body>index
+</html>
